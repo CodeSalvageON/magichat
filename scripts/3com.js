cd6bb62479e301d8c391990dc4a91a2ce4648680
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2022-03-03 20:49:36 -0500

    Update 3com.js

--- scripts/3com.js
+++ scripts/3com.js
@@ -22,3 +22,50 @@ setTimeout(function () {
     }, 3000);
   }, 3000);
 }, 1000);
+
+const creationForm = document.getElementById("creation-form");
+const mailHandle = document.getElementById("mail-handle1");
+const pfpURL = document.getElementById("pfp-url");
+const displayEmail = document.getElementById("display-email");
+
+let mail_thingy = "";
+let mail_pfp = "";
+
+creationForm.onsubmit = function () {
+  event.preventDefault();
+  
+  fetch ("/connect", {
+    method : "POST",
+    headers : {
+      "Content-Type" : "application/json"
+    },
+    body : JSON.stringify(mailHandle.value)
+  })
+  .then(response => response.text())
+  .then(data => {
+    mail_thingy = mailHandle.value;
+    mail_pfp = pfpURL.value;
+    $("#creation-form").hide();
+    console.log(data);
+    
+    const randMail = Math.floor(Math.random() * 3);
+    let provRand = "";
+    
+    if (randMail === 0) {
+      provRand = "compuserve.com";
+    }
+    
+    else if (randMail === 1) {
+      provRand = "aol.com";
+    }
+    
+    else {
+      provRand = "hotmail.com";
+    }
+    
+    displayEmail.innerText = "Address: " + mail_thingy + "@" + provRand;
+  })
+  .catch(error => {
+    throw error;
+  })
+}
